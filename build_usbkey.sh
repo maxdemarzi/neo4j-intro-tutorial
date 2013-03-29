59b7a3490abb8ff551c0114f6d251a3e40ff6ade
Chris Leishman <chris@leishman.org>
2013-03-29 09:41:32 -0700

    Script to create the USB key for students
    Requires MacOS X, and runs as root

---
+++ build_usbkey.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+dist_dir=$(cd "`dirname "$0"`" && pwd)
+volumes_dir=/Volumes
+blank_volume_name="USB DISK"
+target_volume_name="NEO4J"
+
+blank_volume="$volumes_dir/$blank_volume_name"
+target_volume="$volumes_dir/$target_volume_name"
+
+set -x
+
+diskutil renameVolume "$blank_volume" "$target_volume_name"
+sleep 5
+mdutil -i off "$target_volume"
+sleep 3
+rsync -avP "$dist_dir/USBKEY/" "$target_volume/"
+find "$target_volume" -name '.*' -print0 | xargs -0 rm -rf 
+diskutil eject "$target_volume"
